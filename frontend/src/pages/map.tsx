5fcfc3c191b63e9885b41e5e1a2d68084874b7e1
Manuel Dithurbide <manudithur@gmail.com>
2024-02-24 21:53:20 -0300

    feat: map area + only polygon

--- frontend/src/pages/map.tsx
+++ frontend/src/pages/map.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import { GoogleMap, DrawingManager, LoadScript, useLoadScript, useJsApiLoader } from '@react-google-maps/api';
+import React, { useState, useEffect } from 'react';
+import { GoogleMap, DrawingManager, useJsApiLoader } from '@react-google-maps/api';
+import { Button, Text, Title } from '@mantine/core';
 import { Library } from '@googlemaps/js-api-loader';
-import { Button } from '@mantine/core';
 
 const containerStyle = {
   width: '400px',
@@ -13,73 +13,111 @@ const center = {
   lng: -38.523
 };
 
-
 const libraries = ['drawing'] as Library[];
 
-interface LatLng {
-  lat: number;
-  lng: number;
-}
-
 const MapComponent: React.FC = () => {
+  const [polygon, setPolygon] = useState<google.maps.Polygon | null>(null);
+  const [isPolygonDrawn, setIsPolygonDrawn] = useState(false);
+  const [area, setArea] = useState<number | null>(null);
+  const [path, setPath] = useState<google.maps.LatLngLiteral[] | null>(null);
 
-  const [polygons, setPolygons] = useState<LatLng[][]>([]);
-
-  const {isLoaded, loadError} = useJsApiLoader({
-    googleMapsApiKey: 'YOUR_API_KEY',
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY as string,
     libraries,
-  })
+  });
+
+  useEffect(() => {
+    // This effect ensures the polygon is removed from the map when polygon state is set to null
+    return () => {
+      if (polygon) {
+        polygon.setMap(null);
+      }
+    };
+  }, [polygon]); // Depend on the polygon state
+
+  const handleOverlayComplete = (e: google.maps.drawing.OverlayCompleteEvent) => {
+    if (e.type === google.maps.drawing.OverlayType.POLYGON) {
+      const polygon = e.overlay as google.maps.Polygon;
+      setPolygon(polygon);
+      setIsPolygonDrawn(true);
+      setArea( google.maps.geometry.spherical.computeArea(polygon.getPath()) / 10000); // assuming you want to log the area in hectares
+      setPath(polygon.getPath().getArray().map(latlng => ({
+        lat: latlng.lat(),
+        lng: latlng.lng()
+      })));
+    }
+  };
+
+  const resetDrawing = () => {
+    // This directly manipulates the map to remove the polygon
+    if (polygon) {
+      polygon.setMap(null);
+    }
+    setPolygon(null);
+    setIsPolygonDrawn(false);
+    setArea(null);
+    setPath(null);
+  };
 
   return (
     <>
-      <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=drawing"></script>
-      
-        {isLoaded && (
-          <>
+      {isLoaded && (
+        <>
           <GoogleMap
             mapContainerStyle={containerStyle}
             center={center}
             zoom={10}
-            mapTypeId="satellite"
             options={{
               mapTypeControl: false, // Hide the map type control
               mapTypeId: 'satellite', // Set the default map type to satellite
+              mapTypeControlOptions: {
+                style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR, // Set the map type control style to horizontal bar
+                position: google.maps.ControlPosition.TOP_CENTER, // Set the position of the map type control to top center
+              },
             }}
           >
             <DrawingManager
               options={{
-                drawingControl: true,
+                drawingControl: !isPolygonDrawn,
                 drawingControlOptions: {
                   position: google.maps.ControlPosition.TOP_CENTER,
-                  drawingModes: [
-                    google.maps.drawing.OverlayType.POLYGON,
-                  ],
-                }
+                  drawingModes: [google.maps.drawing.OverlayType.POLYGON],
+                },
+                polygonOptions: {
+                  strokeColor: '#00ff52', // Pink color for the polygon lines
+                  strokeOpacity: 0.8,
+                  strokeWeight: 2,
+                  fillColor: '#00ff52',
+                  fillOpacity: 0.35,
+                },
               }}
-              onOverlayComplete={(e) => {
-                if (e.type === google.maps.drawing.OverlayType.POLYGON) {
-                  const newPolygon = e.overlay as google.maps.Polygon;
-                  const path = newPolygon.getPath().getArray().map(latlng => ({
-                    lat: latlng.lat(),
-                    lng: latlng.lng()
-                  }));
-                  setPolygons([...polygons, path]);
-                }
-              }}
-              
+              onOverlayComplete={handleOverlayComplete}
             />
           </GoogleMap>
-          <Button onClick={() => console.log(polygons)}>Log Polygons</Button>
+          <br></br>
+          <Title order={3}>Area</Title>
+          <Text>{area?.toFixed(2)} hectares</Text>
+          <br></br>
           <Button onClick={() => {
-            const areaInSquareMeters = google.maps.geometry.spherical.computeArea(polygons[0]);
-            console.log(areaInSquareMeters/10000);
-          }}>Area</Button>
-          </>
-        )}
-
+            if (polygon) {
+              const path = polygon.getPath().getArray().map(latlng => ({
+                lat: latlng.lat(),
+                lng: latlng.lng()
+              }));
+              console.log(path);
+            }
+          }}>Log Polygon Path</Button>
+          <Button onClick={() => {
+            if (polygon) {
+              const areaInSquareMeters = google.maps.geometry.spherical.computeArea(polygon.getPath());
+              console.log(areaInSquareMeters / 10000); // assuming you want to log the area in hectares
+            }
+          }}>Calculate Area</Button>
+          <Button onClick={resetDrawing}>Reset Drawing</Button>
+        </>
+      )}
     </>
   );
-  
 };
 
 export default MapComponent;
