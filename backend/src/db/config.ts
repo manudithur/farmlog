414a6ebd412a46058040e5a8850c44a6ca7c6e00
MANUEL ESTEBAN DITHURBIDE <mdithurbide@itba.edu.ar>
2024-03-14 16:26:48 -0300

    fix: mongo uri

--- backend/src/db/config.ts
+++ backend/src/db/config.ts
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_URI = 'mongodb://user:password@localhost';
+const MONGO_URI = process.env.MONGO_URI
 
 mongoose.connect(MONGO_URI)
   .then(() => {
