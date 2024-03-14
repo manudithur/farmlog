77f63adef21b20a67131de76f62228edc55f8123
MANUEL ESTEBAN DITHURBIDE <mdithurbide@itba.edu.ar>
2024-03-14 17:33:25 -0300

    test

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,33 +1,3 @@
-import express from 'express';
-import userRoutes from './routes/userRoutes';
-import dotenv from 'dotenv';
-import farmRoutes from './routes/farmRoutes';
-import fieldRoutes from './routes/fieldRoutes';
+import app from '../src/index';
 
-const cors = require('cors');
-
-dotenv.config();
-
-if (!process.env.JWT_SECRET || !process.env.PORT) {
-  console.log('Please define JWT_SECRET, MONGO_URI and PORT in .env file');
-  process.exit(1); // Exit the application if the JWT_SECRET is not defined
-}
-
-
-const app = express();
-app.use(express.json());
-
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running at http://localhost:${process.env.PORT}`);
-});
-
-app.use(cors())
-app.use('/users', userRoutes);
-app.use('/farms', farmRoutes);
-app.use('/fields', fieldRoutes);
+export default app;
