e092a1b44c25e99cea08139135b6f9f4ebcd9a1c
MANUEL ESTEBAN DITHURBIDE <mdithurbide@itba.edu.ar>
2024-03-14 19:26:43 -0300

    test

--- frontend/src/api/config.ts
+++ frontend/src/api/config.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL as string,
-    timeout: 5000,
+    timeout: 100000,
 });
 
 api.interceptors.response.use(
